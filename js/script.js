4dea583a8f35bae56d6a1a6d7b80fecc37f7ccb5
Simone <simone.sichili@gmail.com>
2020-11-19 17:30:20 +0100

    Step #2 User list array. Compare user list and bomb list

--- js/script.js
+++ js/script.js
@@ -17,20 +17,57 @@ var bombList = [];
 while (bombList.length < 16) {
 
     var randomBomb = generateNumber(1, 100);
-    console.log("Numero bomba casuale:", randomBomb);
+    // console.log("Numero bomba casuale:", randomBomb);
     var comparedNumber = isIncluded(randomBomb, bombList);
     
-    if (comparedNumber == true) {
-        bombList.pop(randomBomb);
+    if (comparedNumber == false) {
+        bombList.push(randomBomb);
     } 
-
-    bombList.push(randomBomb);
 
 }
 
 console.log("Array delle bombe:", bombList);
 
 
+// #2 Chiedere all’utente (100 - 16) (84) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+var userList = [].sort();
+checkBomb = false;
+
+while (userList.length < 84 && checkBomb == false) {
+
+    //numeri generati automaticamente
+    // var randomUserNumber = generateNumber(1, 100); 
+
+    //prompt dei numeri da scrivere
+    var randomUserNumber = parseInt(prompt("Inserisci un numero")); 
+    // console.log("Numero utente casuale:", randomUserNumber);
+
+    var comparedUserNumber = isIncluded(randomUserNumber, userList);
+
+    if (comparedUserNumber == false) {
+        userList.push(randomUserNumber);
+    } else if (comparedUserNumber == true) {
+        alert("Hai inserito un numero già esistente");
+    }
+    
+    for (var k = 0; k < bombList.length; k++) {
+        
+        if (randomUserNumber == bombList[k]) {
+            alert("Numero presente. Hai perso!");
+            checkBomb = true;
+        }
+        
+    }
+    
+}
+
+console.log("Array numeri dell'utente:", userList); 
+
+var commonElements = commonElements(userList, bombList);
+
+if (commonElements == false) {
+    alert("Hai vinto!")
+};
 
 
 // FUNCTIONS ---------------------------------------
@@ -55,3 +92,15 @@ function isIncluded(nmb, array) {
     return result;
 
 }
+
+function commonElements(array1, array2) { 
+      
+    for (var i = 0; i < array1.length; i++) {     
+        for (var j = 0; j < array2.length; j++) {     
+            if (array1[i] === array2[j]) { 
+                return true; 
+            } 
+        } 
+    } 
+    return false;  
+} 
